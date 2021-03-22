bf015f7456c9baacb54e1749dfa643cb2aac363b
Maciej Łącki <maciej.lacky@gmail.com>
2021-03-22 19:58:29 +0100

    add search control in admin and ajax code to quick edit

--- CloudAAdmin/Controllers/HomeController.cs
+++ CloudAAdmin/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using CloudA.Data.Data;
 using CloudAAdmin.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -18,23 +19,148 @@ namespace CloudAAdmin.Controllers
     public class HomeController : Controller
     {
         private readonly CloudAContext _context;
-       
-        public HomeController(CloudAContext context)
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private List<Images> photosEvent;
+        private Images photoEvent;
+
+        public HomeController(CloudAContext context, IWebHostEnvironment hostEnvironment)
         {
             _context = context;
-            
+            this._hostEnvironment = hostEnvironment;
+            photosEvent = new List<Images>();
+            photoEvent = new Images();
+
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string searchTerm, string NumOfPeople, bool isDateChange, DateTime dateFrom)
         {
-            ViewBag.ModelEvents =
-                   (
-                       from events in _context.Event
-                       orderby events.DateFrom
-                       select events
-                   ).ToList();
-            return View();
+           
+            if (searchTerm == null && NumOfPeople == null && isDateChange == false)
+            {
+                ViewBag.ModelEvents =
+                 (
+                   from events in _context.Event
+                   orderby events.DateFrom
+                   select events
+                 ).ToList();
+                return View();
+            }
+            else
+            {
+                if (searchTerm != null)
+                {
+                    ViewBag.ModelEvents =
+                  (
+                    from events in _context.Event
+                    where events.TitlePL.StartsWith(searchTerm)
+                    orderby events.DateFrom
+                    select events
+                  ).ToList();
+                    return View();
+                }
+
+                if (isDateChange)
+                {
+                    ViewBag.ModelEvents =
+                  (
+                    from events in _context.Event
+                    where events.DateFrom == dateFrom
+                    select events
+                  ).ToList();
+                    return View();
+                }
+
+                else
+                {
+                    ViewBag.ModelEvents =
+                  (
+                    from events in _context.Event
+                    where events.MaxNumOfPeople == Convert.ToInt32(NumOfPeople)
+                    orderby events.DateFrom
+                    select events
+                  ).ToList();
+                    return View();
+                }
+
+            }
+
+
         }
+        
+        public IActionResult ShowEditItem(int id)
+        {
+            Event @event = _context.Event.FirstOrDefault(x => x.IdEvent == id);
+            return PartialView(@event);
+        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> ShowEditItem(int id, [Bind("IdEvent,TitlePL,TitleEng,TitleRos,DateFrom,DateTo,IsRegister,LogoUrl,Content,MaxNumOfPeople,ImageFile,NumOfRegistered")] Event @event)
+        //{
+        //    if (id != @event.IdEvent)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(@event);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!EventExists(@event.IdEvent))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+
+        //        //creating path to upload multiple images in database
+        //        if (@event.ImageFile != null)
+        //        {
+        //            var list = @event.ImageFile;
+        //            foreach (var item in list)
+        //            {
+        //                //_hostEnvironment.WebRootPath
+
+        //                string wwwRootPath = _hostEnvironment.WebRootPath;
+        //                string fileName = Path.GetFileNameWithoutExtension(item.FileName);
+        //                string extension = Path.GetExtension(item.FileName);
+        //                string pathUrl = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+        //                @event.LogoUrl = pathUrl;
+        //                photoEvent.PathUrl = pathUrl;
+        //                photosEvent.Add(new Images { PathUrl = pathUrl });
+        //                string path = Path.Combine(wwwRootPath + "/Image/", fileName);
+        //                using (var fileStream = new FileStream(path, FileMode.Create))
+        //                {
+        //                    await item.CopyToAsync(fileStream);
+        //                }
+
+        //            }
+
+        //            _context.Update(@event);
+
+        //            var previousIdFromDataBase = id;
+        //            foreach (var item in photosEvent)
+        //            {
+        //                item.IdEvent = previousIdFromDataBase;
+
+
+        //            }
+        //            _context.Image.AddRange(photosEvent);
+        //            await _context.SaveChangesAsync();
+        //        }
+
+
+        //        return RedirectToAction("Index", "Home");
+        //    }
+        //    return View(@event);
+        //}
 
         public IActionResult Privacy()
         {
@@ -46,8 +172,11 @@ namespace CloudAAdmin.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        private bool EventExists(int id)
+        {
+            return _context.Event.Any(e => e.IdEvent == id);
+        }
 
-       
 
     }
 }
--- CloudAAdmin/wwwroot/js/site.js
+++ CloudAAdmin/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    $(".editItemRadio").change(function () {
+        itemUpdate();
+    });
+
+    function itemUpdate() {
+        $.ajax({
+            type: "POST",
+            url: 'Home/ShowEditItem',
+            data: $("form").serialize(),
+            success: function (data) {
+                $("#EditFormArea").html(data);
+            }
+        });
+    };
+
+   
+});
